5b62b8e7ee667f596ce3b417dab12418a7960375
Camden Jones <CamdenJones1280@gmail.com>
2024-03-29 14:19:12 -0500

    added connection pooling part

--- tutorialCommands.py
+++ tutorialCommands.py
@@ -2,16 +2,34 @@
 import sqlalchemy
 
 # connect to a database
-from sqlalchemy import create_engine
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+from sqlalchemy import create_engine, text
+
+# create engine with pooling capabilities
+global my_engine
+my_engine = create_engine(
+    "sqlite+pysqlite:///:memory:",
+    pool_size=5,  # Set the maximum number of connections in the pool
+    max_overflow=10,  # Allow up to 10 additional connections to be created if the pool is exhausted
+    pool_timeout=30,  # Set the timeout for getting a connection from the pool (in seconds)
+    pool_recycle=3600,  # Automatically recycle connections after 1 hour (in seconds)
+)
 
 # simple connection and showing how text works
-with engine.connect() as conn:
+with my_engine.connect() as conn:
   result = conn.execute(text("select 'SQLAlchemy'"))
+  conn.close()
 
 # showing off commit and other sql commands
-with engine.connect() as conn:
+with my_engine.connect() as conn:
+
   conn.execute(text("CREATE TABLE newTable (a int, b text)"))
   conn.execute(text("INSERT INTO newTable (a, b) VALUES (:a, :b)"), [{"a": 1, "b": "hello"}])
   conn.commit()
+  conn.close()
+
+#do an example of using the python syntax for writing queries
+  
+#do an example of session stuff (truthfully IDK how to really do this in an example that makes sense)
+  
+#add an example section here that uses results.fetchall() and results.fetchone(), add to guide as well?
 
